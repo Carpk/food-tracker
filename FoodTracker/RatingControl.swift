a5bc9fd958d77be866f61656c9085d2819e95315
Shawn <zappeln@gmail.com>
2018-01-06 18:13:09 -0600

    create file for custom controls of star ratings

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Shawn Klein on 1/6/18.
+//  Copyright © 2018 Shawn Klein. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
